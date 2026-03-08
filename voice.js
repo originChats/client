d5a94d152f1f0f9230081145db1e9f30a06140d8
Mistium <d.a.kimber@icloud.com>
2026-03-08 20:04:15 +0000

    stop ip leaks

--- voice.js
+++ voice.js
@@ -42,7 +42,30 @@ class VoiceManager {
     initPeerJS() {
         try {
             this.peer = new Peer(null, {
-                debug: 2
+                debug: 2,
+                config: {
+                    iceServers: [
+                        {
+                            urls: 'stun:openrelay.metered.ca:80'
+                        },
+                        {
+                            urls: 'turn:openrelay.metered.ca:80',
+                            username: 'openrelayproject',
+                            credential: 'openrelayproject'
+                        },
+                        {
+                            urls: 'turn:openrelay.metered.ca:443',
+                            username: 'openrelayproject',
+                            credential: 'openrelayproject'
+                        },
+                        {
+                            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                            username: 'openrelayproject',
+                            credential: 'openrelayproject'
+                        }
+                    ],
+                    iceTransportPolicy: "relay"
+                }
             });
 
             this.peer.on('open', (id) => {
